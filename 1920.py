15af27e9265c650fd48878c991b1de20b43084e1
kjh9267 <kjh9267z@naver.com>
2017-11-19 20:29:12 +0900

    save

--- 1920.py
+++ 1920.py
@@ -8,3 +8,24 @@ for i in b:
         print 1
     else:
         print 0
+'''
+def search(arr,len,target):
+    first = 0
+    last = len - 1
+    while first <= last:
+        mid = (first + last) / 2
+        if arr[mid] == target:
+            return 1
+        else:
+            if arr[mid] > target:
+                last = mid - 1
+            else:
+                first = mid + 1
+    return 0
+input()
+a = sorted(map(int,raw_input().split()))
+input()
+b = map(int,raw_input().split())
+for i in range(len(b)):
+    print search(a,len(a),b[i])
+'''
