ba44906ffecc7a908bfb4e44604a7d2666c89a58
kjh9267 <kjh9267z@naver.com>
2017-11-19 20:10:58 +0900

    save

--- 10815.py
+++ 10815.py
@@ -10,3 +10,27 @@ for i in range(len(b)):
     else:
         d.append('0')
 print " ".join(d)
+'''
+def search(arr,len,target):
+    first = 0
+    last = len - 1
+    mid = 0
+    while first <= last:
+        mid = (first + last)/2
+        if arr[mid] == target:
+            return 1
+        else:
+            if arr[mid] > target:
+                last = mid - 1
+            else:
+                first = mid + 1
+    return 0
+n = input()
+a = sorted(map(int,raw_input().split()))
+m = input()
+b = map(int,raw_input().split())
+c = []
+for i in range(len(b)):
+     c.append(search(a,len(a),b[i]))
+print " ".join([str(c[i]) for i in range(len(c))])
+'''
