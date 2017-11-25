8fe9fc07921dff165eb4d401a2258925a5309704
kjh9267 <kjh9267z@naver.com>
2017-11-25 21:44:49 +0900

    save

--- 1932.py
+++ 1932.py
@@ -1,12 +1,13 @@
 n = input()
 x = [map(int,raw_input().split()) for i in range(n)]
-dp = [[0 for u in range(i*2)] for i in range(1,len(x))]
-dp.insert(0,x[0])
-for i in range(1,len(x)):
-    dp[i][0] = x[i][0] + dp[i-1][0]
-    dp[i][2*i-1] = x[i][i] + dp[i-1][2*(i-1)-1]
-for i in range(2,len(x)):
-    for u in range(1,i*2-1,2):
-        '''dp[i][u] = dp[i-1][u-1] + x[i][]
-        dp[i][u+1] = dp[i-1][u] + x[i][]'''
-        print dp
+dp = [[0 for j in range(n)] for i in range(n)]
+dp[0][0] = x[0][0]
+for i in range(1,n):
+    for j in range(len(x[i])):
+        if j-1 < 0:
+            dp[i][j] = dp[i - 1][ j] + x[i][j]
+        elif j == len(x):
+            dp[i][ j] = dp[i - 1][ j - 1] + x[i][j]
+        else:
+            dp[i][j] = max(dp[i-1][j-1]+x[i][j],dp[i-1][j]+x[i][j])
+print max(dp[n-1])
