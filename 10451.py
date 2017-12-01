259a5ae96493f04960edf9e6b1481d48c5043937
kjh9267 <intellijunho@kakao.com>
2017-12-01 16:34:05 +0900

    save

---
+++ 10451.py
@@ -0,0 +1,16 @@
+for i in range(input()):
+    n = input()
+    a = range(1,n+1)
+    b = map(int,raw_input().split())
+    visit = []
+    cnt = 0
+    for j in range(n):
+        if a[j] not in visit:
+            queue = [a[j]]
+            while len(queue) is not 0:
+                x = queue.pop(0)
+                visit.append(x)
+                if b[a.index(x)] not in visit:
+                    queue.append(b[a.index(x)])
+            cnt += 1
+    print cnt
