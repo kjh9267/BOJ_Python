202b2d955ddfd534c4a301765f12997971c9dbd3
kjh9267 <kjh9267z@naver.com>
2017-12-03 18:39:49 +0900

    save

--- 2178.py
+++ 2178.py
@@ -4,20 +4,20 @@ def bfs(graph,visit):
     cnt = 0
     while len(queue) is not 0:
         for i in range(len(queue)):
-            visit.append(queue[i])
+            visit[queue[i][0]-1][queue[i][1]-1] = 1
             if [n, m] in queue:
                 break
             if queue[i][0] - 1 >= 1:
-                if graph[queue[i][0]-2][queue[i][1]-1] == 1 and [queue[i][0] - 1, queue[i][1]] not in visit and [queue[i][0] - 1,queue[i][1]] not in level:
+                if graph[queue[i][0]-2][queue[i][1]-1] == 1 and visit[queue[i][0]-2][queue[i][1]-1] == 0 and [queue[i][0] - 1,queue[i][1]] not in level:
                     level.append([queue[i][0] - 1,queue[i][1]])
             if queue[i][1] - 1 >= 1:
-                if graph[queue[i][0]-1][queue[i][1]-2] == 1 and [queue[i][0], queue[i][1]-1] not in visit and [queue[i][0],queue[i][1]-1] not in level:
+                if graph[queue[i][0]-1][queue[i][1]-2] == 1 and visit[queue[i][0]-1][queue[i][1]-2] == 0 and [queue[i][0],queue[i][1]-1] not in level:
                     level.append([queue[i][0],queue[i][1]-1])
             if queue[i][0] + 1 <= len(graph):
-                if graph[queue[i][0]][queue[i][1]-1] == 1 and [queue[i][0] + 1, queue[i][1]] not in visit and [queue[i][0] + 1,queue[i][1]] not in level:
+                if graph[queue[i][0]][queue[i][1]-1] == 1 and visit[queue[i][0]][queue[i][1]-1] == 0 and [queue[i][0] + 1,queue[i][1]] not in level:
                     level.append([queue[i][0] + 1,queue[i][1]])
             if queue[i][1] + 1 <= len(graph[0]):
-                if graph[queue[i][0]-1][queue[i][1]] == 1 and [queue[i][0], queue[i][1]+1] not in visit and [queue[i][0],queue[i][1]+1] not in level:
+                if graph[queue[i][0]-1][queue[i][1]] == 1 and visit[queue[i][0]-1][queue[i][1]] == 0 and [queue[i][0],queue[i][1]+1] not in level:
                     level.append([queue[i][0],queue[i][1]+1])
         queue = []
         if len(queue) is 0:
@@ -27,5 +27,5 @@ def bfs(graph,visit):
     return cnt
 n, m = map(int,raw_input().split())
 graph = [map(int,list(raw_input())) for i in range(n)]
-visit = []
+visit = [[0 for j in range(m)] for i in range(n)]
 print bfs(graph,visit)
