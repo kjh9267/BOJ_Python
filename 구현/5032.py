402f4bc0397504fec6c6432fded82e72fd0a8fa8
kjh9267 <intellijunho@kakao.com>
2017-12-22 20:32:34 +0900

    Python3

--- 구현/5032.py
+++
@@ -1,7 +0,0 @@
-a, b ,c = map(int,raw_input().split())
-cnt = (a+b)/c
-empty = (a+b)/c + (a+b)%c
-while empty/c >= 1:
-    cnt += empty/c
-    empty = empty/c + empty%c
-print cnt
--- 문자열 처리/1159.py
+++
@@ -1,9 +0,0 @@
-x = [list(raw_input())[0] for i in range(input())]
-z = []
-for y in set(x):
-    if x.count(y) > 4:
-        z.append(y)
-if len(z) != 0:
-    print "".join(sorted(z))
-else:
-    print "PREDAJA"
