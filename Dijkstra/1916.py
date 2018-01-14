03aaa3740887f192db2ba0f8cb47d71a2bf5af75
kjh9267 <intellijunho@kakao.com>
2018-01-14 19:30:30 +0900

    Python3

---
+++ Dijkstra/1916.py
@@ -0,0 +1,8 @@
+import sys
+from queue import PriorityQueue
+
+n = int(sys.stdin.readline())
+m = int(sys.stdin.readline())
+
+for _ in range(m):
+    a, b, c = map(int,sys.stdin.readline().split())
