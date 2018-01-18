f4940113891c0751fd621cd5027d096c01f79b12
kjh9267 <intellijunho@kakao.com>
2018-01-18 22:56:53 +0900

    Python3

--- Network Flow/6086.py
+++ Network Flow/6086.py
@@ -13,7 +13,6 @@ n = int(sys.stdin.readline())
 inf = float('inf')
 capacity = [[0 for j in range(52)] for i in range(52)]
 flow = [[0 for j in range(52)] for i in range(52)]
-visit = []
 total = 0
 
 for _ in range(n):
@@ -23,30 +22,34 @@ for _ in range(n):
     w = int(w)
     capacity[u][v] += w
     capacity[v][u] += w
-print(capacity)
-print(flow)
+
 while True:
     queue = deque()
     queue.append(0)
-    way = [0 for _ in range(52)]
+    way = [-1 for _ in range(52)]
     while queue:
+        key = False
         x = queue.popleft()
-        visit.append(x)
         for i in range(52):
-            if capacity[x][i] - flow[x][i] > 0 and i not in visit:
+            if capacity[x][i] - flow[x][i] > 0 and way[i] is -1:
                 way[x] = i
                 queue.append(i)
                 if i is 25:
+                    key = True
                     break
-        f = inf
-        for i, j in enumerate(way):
-            if j is not 0:
-                if f > capacity[i][j] - flow[i][j]:
-                    f = capacity[i][j] - flow[i][j]
-        for i, j in enumerate(way):
-            if j is not 0:
-                flow[i][j] += f
-                flow[j][i] -= f
-        total += f
-    print(total)
-    break
+        if key is True:
+            break
+    if 25 not in way:
+        break
+    f = inf
+    for i, j in enumerate(way):
+        if j is not -1:
+            if f > capacity[i][j] - flow[i][j]:
+                f = capacity[i][j] - flow[i][j]
+    for i, j in enumerate(way):
+        if j is not -1:
+            flow[i][j] += f
+            flow[j][i] -= f
+    total += f
+
+print(total)
