0d550e932d5e80cdd16c674c39a504b354a36848
kjh9267 <intellijunho@kakao.com>
2018-02-02 20:59:36 +0900

    Python3

--- Simulation/2573.py
+++ Simulation/2573.py
@@ -1,70 +1,67 @@
 import sys
-def bfs(start):
-    queue = [start]
-    while len(queue) is not 0:
-        x = queue.pop(0)
-        visit[x[0]][x[1]] = 1
-        if 0 <= x[0]-1:
-            if graph[x[0]-1][x[1]] > 0:
-                if visit[x[0]-1][x[1]] == 0:
-                    if [x[0]-1,x[1]] not in queue:
-                        queue.append([x[0]-1,x[1]])
-        if 0 <= x[1]-1:
-            if graph[x[0]][x[1]-1] > 0:
-                if visit[x[0]][x[1]-1] == 0:
-                    if [x[0],x[1]-1] not in queue:
-                        queue.append([x[0],x[1]-1])
-        if x[0]+1 < n:
-            if graph[x[0]+1][x[1]] > 0:
-                if visit[x[0]+1][x[1]] == 0:
-                    if [x[0]+1,x[1]] not in queue:
-                        queue.append([x[0]+1,x[1]])
-        if x[1]+1 < m:
-            if graph[x[0]][x[1]+1] > 0:
-                if visit[x[0]][x[1]+1] == 0:
-                    if [x[0],x[1]+1] not in queue:
-                        queue.append([x[0],x[1]+1])
+from collections import deque
+
+
+def bfs(a,b):
+    queue = deque()
+    queue.append([a,b])
+    while queue:
+        z = queue.popleft()
+        visit[z[0]][z[1]] = 1
+        for i in range(4):
+            y = z[0] + dy[i]
+            x = z[1] + dx[i]
+            if 0 <= y < n and 0 <= x < m:
+                if graph[y][x] > 0 and visit[y][x] is 0 and [y,x] not in queue:
+                    queue.append([y,x])
+
+
 def melt(a,b):
-    if 0 <= a - 1:
-        if graph[a-1][b] <= 0:
-            melvisit[a][b] += 1
-    if n > a + 1:
-        if graph[a+1][b] <= 0:
-            melvisit[a][b] += 1
-    if 0 <= b - 1:
-        if graph[a][b-1] <= 0:
-            melvisit[a][b] += 1
-    if m > b + 1:
-        if graph[a][b+1] <= 0:
-            melvisit[a][b] += 1
+    for i in range(4):
+        y = a + dy[i]
+        x = b + dx[i]
+        if 0 <= y < n and 0 <= x < m :
+            if graph[y][x] <= 0:
+                melvisit[a][b] += 1
+
+
 n, m = map(int,sys.stdin.readline().split())
-graph = [map(int,sys.stdin.readline().split()) for i in range(n)]
+graph = [list(map(int,sys.stdin.readline().split())) for _ in range(n)]
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
 cnt = 0
 land = 0
+
 while True:
     melvisit = [[0 for j in range(m)] for i in range(n)]
     for i in range(n):
         for j in range(m):
             if graph[i][j] > 0:
                 melt(i,j)
+
     for i in range(n):
         for j in range(m):
             graph[i][j] -= melvisit[i][j]
+
     visit = [[0 for j in range(m)]for i in range(n)]
     land = 0
+
     for i in range(n):
         for j in range(m):
-            if graph[i][j] > 0 and visit[i][j] == 0:
-                bfs([i,j])
+            if graph[i][j] > 0 and visit[i][j] is 0:
+                bfs(i,j)
                 land += 1
+
     cnt += 1
     if land > 1:
-        print cnt
+        print(cnt)
         break
+
     for i in range(n):
         for j in range(m):
             if graph[i][j] < 0:
                 graph[i][j] = 0
+
     if sum([sum(graph[i]) for i in range(n)]) is 0:
-        print 0
+        print(0)
         break
