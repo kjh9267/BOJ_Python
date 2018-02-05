79065823b23324aafe9dea5e2e92fb6a3321e54f
kjh9267 <intellijunho@kakao.com>
2018-02-05 18:53:33 +0900

    Python3

--- Dijkstra/1753.py
+++ Dijkstra/1753.py
@@ -15,20 +15,17 @@ for _ in range(e):
     cost[a-1].append(c)         # 간선의 비용
 
 pq = PriorityQueue()      # 우선순위 큐
+pq.put((0,k-1))         # 우선순위 큐에 시작점만 거리를 0으로 하여 넣어준다
 res = [inf for _ in range(v)]   # 결과 값을 저장하기 위한 list
-res[k-1] = 0
-
-for i in range(v):
-    if i is k - 1:
-        pq.put((0, i))        # 출발 노드는 거리를 0으로 하여 우선순위 큐에서 제일 먼저 나오게 한다
-    else:
-        pq.put((inf, i))      # 출발 노드를 제외한 모든 곳은 무한대 값
+res[k-1] = 0            # 출발 점의 결과 값은 0으로 초기화
 
 while pq.qsize():                 # 우선순위 큐가 빌 때까지 반복
     dist, node = pq.get()         # 최소거리와 그 노드를 우선순위큐에서 get
+    if res[node] < dist:        # 이미 나온 결과 값 보다 더 큰 비용이 들어온 경우 우선순위 큐에서 다시 get
+        continue
     for i, j in zip(graph[node], cost[node]):   # 간선의 도착점과 그에 대한 비용
-        if res[i] > j + res[node]:      # 도착점의 비용과 도착점의 비용 + 전 노드까지의 값을 비교
-            res[i] = j + res[node]      # 도착점의 비용이 더 클 경우 도착점의 비용 + 전 노드까지의 값으로 변경
+        if res[i] > j + dist:      # 도착점의 비용과 도착점의 비용 + 전 노드까지의 값을 비교
+            res[i] = j + dist      # 도착점의 비용이 더 클 경우 도착점의 비용 + 전 노드까지의 값으로 변경
             pq.put((res[i], i))         # 갱신된 값을 우선순위 큐에도 넣어준다
 
 for i in res:
