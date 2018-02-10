6a33310d5f761fd78110f1a2c62897d8ec2e3a38
kjh9267 <intellijunho@kakao.com>
2018-02-10 20:10:23 +0900

    Python3

--- Dynamic Programming/10844.py
+++ Dynamic Programming/10844.py
@@ -10,6 +10,6 @@ for i in range(1,n):
     dp[i][0] = dp[i-1][1]
     dp[i][9] = dp[i-1][8]
     for j in range(1,9):
-        dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1]) % 1000000000
+        dp[i][j] = (dp[i-1][j-1] % 1000000000 + dp[i-1][j+1] % 1000000000) % 1000000000
 
-print(sum(dp[n-1]))
+print(sum(dp[n-1]) % 1000000000)
