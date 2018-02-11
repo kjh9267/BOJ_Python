edfe8a75da2cafd890bf5e93bd60c9876879cfdb
kjh9267 <intellijunho@kakao.com>
2018-02-11 16:41:56 +0900

    Python3

--- Dynamic Programming/1463.py
+++ Dynamic Programming/1463.py
@@ -2,15 +2,16 @@ import sys
 
 n = int(sys.stdin.readline())
 inf = float('inf')
-dp = [inf for _ in range(n+1)]
+dp = [inf for _ in range(1000001)]
 dp[1] = 0
-
-for i in range(1,n+1):
-    if i+1 <= n and dp[i+1] > dp[i] + 1:
-        dp[i+1] = dp[i] + 1
-    if i*2 <= n and dp[i*2] > dp[i] + 1:
-        dp[i*2] = dp[i] + 1
-    if i*3 <= n and dp[i*3] > dp[i] + 1:
-        dp[i*3] = dp[i] + 1
-
+dp[2] = 1
+dp[3] = 1
+for i in range(4,n+1):
+    dp[i] = dp[i-1] + 1
+    if i%2 == 0:
+        if dp[i] > dp[i//2] + 1:
+            dp[i] = dp[i//2] + 1
+    if i%3 == 0:
+        if dp[i] > dp[i//3] + 1:
+            dp[i] = dp[i//3] + 1
 print(dp[n])
