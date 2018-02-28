e841e7c8aa548f15fa304b5f8dfddcdf46217f58
kjh9267 <intellijunho@kakao.com>
2018-02-28 19:54:16 +0900

    Python3

--- String Handling/1100.py
+++ String Handling/1100.py
@@ -1,11 +1,14 @@
-a = [raw_input() for i in range(8)]
+a = [input() for i in range(8)]
 cnt = 0
+
 for i in range(0,8,2):
     for u in range(0,8,2):
         if a[i][u] == 'F':
             cnt += 1
+
 for i in range(1,8,2):
     for u in range(1,8,2):
         if a[i][u] == 'F':
             cnt += 1
-print cnt
+
+print(cnt)
