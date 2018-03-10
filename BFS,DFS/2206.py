32b9877bfebde2e4ed33184eceacb0dc2aa089ae
kjh9267 <intellijunho@kakao.com>
2018-03-10 18:13:54 +0900

    Python3

---
+++ BFS,DFS/2206.py
@@ -0,0 +1,51 @@
+import sys
+from collections import deque
+
+
+def bfs():
+    queue = deque()
+    queue.append((0,0,0))
+    level = deque()
+    visit[0][0] = 1
+    cnt = 1
+    while queue:
+        a = queue.popleft()
+        for i, j in zip(dx,dy):
+            x = i + a[0]
+            y = j + a[1]
+            if 0 <= x < m and 0 <= y < n:
+                if x == m-1 and y == n-1:
+                    return cnt + 1
+                if a[2] is 0 and visit[y][x] is 0:
+                    if graph[y][x] is 1:
+                        level.append((x,y,1))
+                        visit2[y][x] = 1
+                    else:
+                        level.append((x,y,0))
+                        visit[y][x] = 1
+                elif a[2] is 1 and visit2[y][x] is 0 and graph[y][x] is 0:
+                    level.append((x,y,1))
+                    visit2[y][x] = 1
+        if not queue:
+            queue.extend(level)
+            level.clear()
+            cnt += 1
+
+
+n, m = map(int,sys.stdin.readline().split())
+graph = [list(map(int,sys.stdin.readline().rstrip())) for _ in range(n)]
+visit = [[0 for j in range(m)] for i in range(n)]
+visit2 = [[0 for j in range(m)] for i in range(n)]
+dx = (1,0,-1,0)
+dy = (0,1,0,-1)
+
+if n is 1 and m is 1:
+    print(0)
+    exit()
+
+res = bfs()
+
+if res is None:
+    print(-1)
+else:
+    print(res)
