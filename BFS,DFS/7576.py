ce47edd9442a602c6d00568a7d3d082fa5cee29a
kjh9267 <intellijunho@kakao.com>
2018-03-11 16:57:31 +0900

    Python3

--- BFS,DFS/7576.py
+++ BFS,DFS/7576.py
@@ -1,43 +1,45 @@
-def bfs(graph, visit, start):
+import sys
+from collections import deque
+
+
+def bfs(tomato):
+    queue = deque()
+    queue.extend(tomato)
+    level = deque()
     cnt = 0
-    queue = start
-    level = []
-    while len(queue) is not 0:
-        for i in range(len(queue)):
-            if queue[i][0]-1 >= 0 and graph[queue[i][0]-1][queue[i][1]] == 0:
-                visit[queue[i][0] - 1][queue[i][1]] = 1
-                level.append([queue[i][0]-1, queue[i][1]])
-            if queue[i][1]-1 >= 0 and graph[queue[i][0]][queue[i][1]-1] == 0:
-                visit[queue[i][0]][queue[i][1] - 1] = 1
-                level.append([queue[i][0], queue[i][1] -1])
-            if queue[i][0]+1 < n and graph[queue[i][0]+1][queue[i][1]] == 0:
-                visit[queue[i][0] + 1][queue[i][1]] = 1
-                level.append([queue[i][0]+1, queue[i][1]])
-            if queue[i][1]+1 < m and graph[queue[i][0]][queue[i][1]+1] == 0:
-                visit[queue[i][0]][queue[i][1] + 1] = 1
-                level.append([queue[i][0], queue[i][1]+1])
-        queue = []
-        graph = visit
-        if len(queue) is 0:
-            queue = level
-            level = []
+    while queue:
+        a = queue.popleft()
+        for i, j in zip(dx,dy):
+            x = i + a[0]
+            y = j + a[1]
+            if 0 <= x < m and 0 <= y < n:
+                if graph[y][x] is 0:
+                    level.append((x,y))
+                    graph[y][x] = 1
+        if not queue:
+            queue.extend(level)
+            level.clear()
             cnt += 1
     return cnt - 1
-m, n = map(int,raw_input().split())
-graph = [map(int,raw_input().split()) for i in range(n)]
-visit = [[graph[i][j] for j in range(m)]for i in range(n)]
-res = [[graph[i][j] for j in range(m)]for i in range(n)]
+
+
+m, n = map(int,sys.stdin.readline().split())
+graph = [list(map(int,sys.stdin.readline().split())) for i in range(n)]
+visit = [[0 for j in range(m)] for i in range(n)]
+dx = (1,0,-1,0)
+dy = (0,1,0,-1)
+tomato = deque()
+
 for i in range(n):
     for j in range(m):
-        if graph[i][j] == 0:
-            res[i][j] = 1
-start = []
-for i in range(n):
-    for j in range(m):
-        if graph[i][j] == 1:
-            start.append([i,j])
-a = bfs(graph,visit,start)
-if visit != res:
-    print -1
-else:
-    print a
+        if graph[i][j] is 1:
+            tomato.append((j,i))
+
+res = bfs(tomato)
+
+for i in graph:
+    if 0 in i:
+        print(-1)
+        exit()
+
+print(res)
