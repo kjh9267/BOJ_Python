112ab054f65d6dee1f2da00d5ff598c2d7b3a3f2
kjh9267 <intellijunho@kakao.com>
2018-03-24 18:05:49 +0900

    Python3

--- 1726.py
+++ 1726.py
@@ -6,21 +6,77 @@ def bfs():
     queue = deque()
     queue.append(start)
     level = deque()
-    visit[start[0]][start[1]] = 1
+    visit[start[0]][start[1]] = [1,1,1]
+    cnt = 0
     while queue:
-        x, y, d = queue.popleft()
+        y, x, d, z = queue.popleft()
         for i, j in enumerate(zip(dx,dy)):
-            xx = x + j[0]
-            yy = y + j[1]
-            if 0 <= xx < m and 0 <= yy < n:
-                if visit[yy][xx] is 0 and graph[yy][xx] == '0':
+            print(list(enumerate(zip(dx,dy))))
+            for u in range(3):
+                xx = x + j[0][u]
+                yy = y + j[1][u]
+                if 0 <= xx < n and 0 <= yy < m:
+                    if graph[yy][xx] == '1':
+                        break
+                    if visit[yy][xx][u] is 0 and graph[yy][xx] == '0':
+                        visit[yy][xx][u] = 1
+                        level.append((yy,xx,i,z + move(d,i)))
+                        if xx == end[1] and y == end[0]:
+                            print(cnt)
+                            print(z)
+                            print(move(d,end[2]))
+                            return cnt + z + move(d, end[2])
+        if not queue:
+            queue.extend(level)
+            level.clear()
+            cnt += 1
+        print(queue)
 
 
+def move(s,e):
+    if s is 0:
+        if e is 0:
+            return 0
+        if e is 2 or e is 3:
+            return 1
+        if e is 1:
+            return 2
+    if s is 1:
+        if e is 1:
+            return 0
+        if e is 2 or e is 3:
+            return 1
+        if e is 0:
+            return 2
+    if s is 2:
+        if e is 2:
+            return 0
+        if e is 0 or e is 1:
+            return 1
+        if e is 3:
+            return 2
+    if s is 3:
+        if e is 3:
+            return 0
+        if e is 0 or e is 1:
+            return 1
+        if e is 2:
+            return 2
 
-n, m = map(int,sys.stdin.readline().split())
-graph = [sys.stdin.readline().split() for _ in range(n)]
-start = tuple(map(int,sys.stdin.readline().split()))
-end = tuple(map(int,sys.stdin.readline().split()))
-dx = (1,-1,0,0)
-dy = (0,0,1,-1)
-visit = [[0 for j in range(m)] for i in range(n)]
+
+m, n = map(int,sys.stdin.readline().split())
+graph = [sys.stdin.readline().split() for _ in range(m)]
+start = list(map(int,sys.stdin.readline().split()))
+start.append(1)
+end = list(map(int,sys.stdin.readline().split()))
+dx = ((1,2,3),(-1,-2,-3),(0,0,0),(0,0,0))
+dy = ((0,0,0),(0,0,0),(1,2,3),(-1,-2,-3))
+visit = [[[0,0,0] for j in range(n)] for i in range(m)]
+
+for i in range(4):
+    start[i] -= 1
+
+for i in range(3):
+    end[i] -= 1
+print(start, end)
+print(bfs())
