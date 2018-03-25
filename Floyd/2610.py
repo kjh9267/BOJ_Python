85f4cb4cbcaa074f38722e5ced20cc52b0ba24c8
kjh9267 <intellijunho@kakao.com>
2018-03-25 15:59:39 +0900

    Python3

--- Floyd/2610.py
+++ Floyd/2610.py
@@ -1,39 +1,60 @@
 import sys
+from collections import deque
+
+
+def bfs():
+    queue = deque()
+    visit = [0 for _ in range(n)]
+    for u in range(n):
+        if visit[u] is 0:
+            queue.append(u)
+            team = [u]
+            while queue:
+                x = queue.popleft()
+                for i, j in enumerate(graph[x]):
+                    if i is u:
+                        continue
+                    if j is 1 and visit[i] is 0:
+                        queue.append(i)
+                        visit[i] = 1
+                        team.append(i)
+            teams.append(team)
+
+
 n = int(sys.stdin.readline())
 m = int(sys.stdin.readline())
 inf = float('inf')
-graph = [[inf for j in xrange(n)]for i in xrange(n)]
-for _ in xrange(m):
+graph = [[inf for __ in range(n)] for _ in range(n)]
+teams = []
+
+for _ in range(m):
     a, b = map(int,sys.stdin.readline().split())
     graph[a-1][b-1] = 1
     graph[b-1][a-1] = 1
-visit = []
-team = []
-for i in xrange(n):
-    y = []
-    if i not in visit:
-        queue = [i]
-    while len(queue) is not 0:
-        x = queue.pop(0)
-        visit.append(x)
-        y.append(x)
-        for j in xrange(n):
-            if graph[x][j] is 1 and j not in visit and j not in queue:
-                queue.append(j)
-    if len(y) is not 0:
-        team.append(y)
-for v in xrange(n):
-    for i in xrange(n):
-        for j in xrange(n):
-            if i is not j:
-                if graph[i][v] + graph[v][j] < graph[i][j]:
-                    graph[i][j] = graph[i][v] + graph[v][j]
-z = [[graph[i][j] for j in xrange(n) if graph[i][j] is not inf] for i in xrange(n)]
+
+bfs()
+
+for v in range(n):
+    for i in range(n):
+        for j in range(n):
+            if i == j:
+                continue
+            if graph[i][j] > graph[i][v] + graph[v][j]:
+                graph[i][j] = graph[i][v] + graph[v][j]
+
 for i in range(n):
-    if len(z[i]) is 0:
-        z[i] = [0]
-res = [[max(z[team[i][j]]) for j in xrange(len(team[i]))]for i in xrange(len(team))]
-print len(res)
-end = sorted([team[i][(res[i].index(min(res[i])))] + 1 for i in xrange(len(res))])
-for i in range(len(end)):
-    print end[i]
+    for j in range(n):
+        if graph[i][j] == inf:
+            graph[i][j] = 0
+
+res = [[max(graph[j]) for j in i if graph[j]] for i in teams]
+output = []
+
+print(len(teams))
+
+for i in range(len(teams)):
+    x = min(res[i])
+    output.append(teams[i][res[i].index(x)] + 1)
+
+for i in sorted(output):
+    print(i)
