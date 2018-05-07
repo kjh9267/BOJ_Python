6e407c08077445ae2a95851750f51764d902a6eb
kjh9267 <intellijunho@kakao.com>
2018-05-07 19:29:43 +0900

    Python3

--- Simulation/3190.py
+++ Simulation/3190.py
@@ -3,17 +3,36 @@ from collections import deque
 
 
 def move(direction):
-    if b is 'D':
+    if b == 'D':
         if direction is 3:
             direction -= 3
         else:
             direction += 1
-    elif b is 'L':
+    elif b == 'L':
         if direction is 0:
             direction += 3
         else:
             direction -= 1
     return direction
+
+
+def go(cnt):
+    cnt += 1
+    head = snake[-1]
+    tail = snake[0]
+    y = head[0] + dy[direction]
+    x = head[1] + dx[direction]
+    if 0 <= y < n and 0 <= x < n and graph[y][x] != 's':
+        if graph[y][x] == 'x':
+            graph[tail[0]][tail[1]] = 'x'
+            snake.popleft()
+        snake.append([y, x])
+        graph[y][x] = 's'
+    else:
+        print(cnt)
+        exit()
+    return cnt
+
 
 n = int(stdin.readline())
 k = int(stdin.readline())
@@ -35,20 +54,9 @@ l = int(stdin.readline())
 for _ in range(l):
     a, b = stdin.readline().split()
     a = int(a) - cnt
-    for i in range(a):
-        cnt += 1
-        head = snake[-1]
-        tail = snake[0]
-        y = head[0] + dy[direction]
-        x = head[1] + dx[direction]
-        if 0 <= y < n and 0 <= x < n and graph[y][x] is not 's':
-            if graph[y][x] is 'x':
-                graph[tail[0]][tail[1]] = 'x'
-                snake.popleft()
-            snake.append([y,x])
-            graph[y][x] = 's'
-        else:
-            print(cnt)
-            exit()
+    for __ in range(a):
+        cnt = go(cnt)
     direction = move(direction)
-print(cnt+1)
+else:
+    while True:
+        cnt = go(cnt)
