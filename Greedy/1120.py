9f837da5dc3bcaa1e79dfa4e24c0aa9848b48f20
kjh9267 <intellijunho@kakao.com>
2018-07-06 19:17:35 +0900

    Python3:

---
+++ Greedy/1120.py
@@ -0,0 +1,17 @@
+import sys
+
+
+a, b = sys.stdin.readline().split()
+length = len(a)
+diff = len(b) - len(a) + 1
+result = float('inf')
+
+
+for i in range(diff):
+    cnt = 0
+    for j in range(length):
+        if a[j] != b[j + i]:
+            cnt += 1
+    result = min(result,cnt)
+
+print(result)
