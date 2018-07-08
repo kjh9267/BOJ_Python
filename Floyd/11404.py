1b500f838263d9b462dd79e98b025ff65ccb4523
kjh9267 <intellijunho@kakao.com>
2018-07-08 18:28:57 +0900

    Python3

--- Floyd/11404.py
+++ Floyd/11404.py
@@ -1,17 +1,28 @@
-n = input()
-m = input()
-x = [[10000000 for u in range(n)]for i in range(n)]
-for i in range(len(x)):
-    x[i][i] = 0
-for i in range(m):
-    a, b, c = map(int,raw_input().split())
-    x[a-1][b-1] = min(c, x[a-1][b-1])
+from collections import deque
+import sys
+
+n = int(sys.stdin.readline())
+m = int(sys.stdin.readline())
+inf = float('inf')
+graph = [[inf for __ in range(n)] for _ in range(n)]
+
+for _ in range(m):
+    a, b, c = map(int,sys.stdin.readline().split())
+    if graph[a-1][b-1] > c:
+        graph[a-1][b-1] = c
+
+for v in range(n):
+    for s in range(n):
+        for e in range(n):
+            if s == e:
+                continue
+            if graph[s][e] > graph[s][v] + graph[v][e]:
+                graph[s][e] = graph[s][v] + graph[v][e]
+
 for i in range(n):
-    for u in range(n):
-        for o in range(n):
-            x[u][o] = min(x[u][o], x[u][i] + x[i][o])
-for i in range(len(x)):
-    for u in range(len(x)):
-        x[i][u] = str(x[i][u])
-for i in range(len(x)):
-    print " ".join(x[i])
+    for j in range(n):
+        if graph[i][j] == inf:
+            graph[i][j] = 0
+
+for i in graph:
+    print(" ".join(map(str,i)))
