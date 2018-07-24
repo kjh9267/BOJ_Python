a03f19ae44acb9bed7b3a31c9e49651a6ef2c75b
kjh9267 <intellijunho@kakao.com>
2018-07-24 17:40:54 +0900

    Python3

--- String Handling/1764.py
+++ String Handling/1764.py
@@ -1,12 +1,10 @@
-n, m = map(int,raw_input().split())
-a = [raw_input() for i in range(n)]
-for i in range(m):
-    a.append(raw_input())
-a = sorted(a)
-b = []
-for i in range(1,len(a)):
-    if (a[i]) == (a[i-1]):
-        b.append(a[i])
-print len(b)
-for i in b:
-    print i
+import sys
+
+n, m = map(int,sys.stdin.readline().split())
+a = [sys.stdin.readline().rstrip() for _ in range(n)]
+b = [sys.stdin.readline().rstrip() for _ in range(m)]
+res = set(a).intersection(b)
+print(len(res))
+
+for i in sorted(res):
+    print(i)
