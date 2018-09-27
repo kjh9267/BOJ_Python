2becb1c4e3e120228a4820f1caedb39d617df75c
kjh9267 <intellijunho@kakao.com>
2018-09-27 19:31:43 +0900

    BFS

--- BFS/7576.py
+++ BFS/7576.py
@@ -8,18 +8,17 @@ def bfs(tomato):
     level = deque()
     cnt = 0
     while queue:
-        a = queue.popleft()
-        for i, j in zip(dx,dy):
-            x = i + a[0]
-            y = j + a[1]
-            if 0 <= x < m and 0 <= y < n:
-                if graph[y][x] is 0:
-                    level.append((x,y))
-                    graph[y][x] = 1
-        if not queue:
-            queue.extend(level)
-            level.clear()
-            cnt += 1
+        size = len(queue)
+        for _ in range(size):
+            cur = queue.popleft()
+            for i, j in zip(dx,dy):
+                x = i + cur[0]
+                y = j + cur[1]
+                if 0 <= x < m and 0 <= y < n:
+                    if graph[y][x] is 0:
+                        queue.append((x,y))
+                        graph[y][x] = 1
+        cnt += 1
     return cnt - 1
 
 
