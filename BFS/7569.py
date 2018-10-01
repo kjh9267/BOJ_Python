e6774b42a455fc0490b8a8de6caf4a60c0922718
kjh9267 <intellijunho@kakao.com>
2018-10-01 22:48:28 +0900

    BFS

--- BFS/7569.py
+++ BFS/7569.py
@@ -7,8 +7,7 @@ dx = (1,0,-1,0,0,0)
 dy = (0,1,0,-1,0,0)
 dz = (0,0,0,0,1,-1)
 queue = deque()
-level = []
-cnt = 0
+cnt = -1
 
 for i in range(h):
     for j in range(n):
@@ -17,18 +16,17 @@ for i in range(h):
                 queue.append((u,j,i))
 
 while queue:
-    cur = queue.popleft()
-    for i, j, u in zip(dx,dy,dz):
-        x = cur[0] + i
-        y = cur[1] + j
-        z = cur[2] + u
-        if 0 <= x < m and 0 <= y < n and 0 <= z < h and graph[z][y][x] is 0:
-            graph[z][y][x] = 1
-            level.append((x,y,z))
-    if not queue and level:
-        queue.extend(level)
-        level.clear()
-        cnt += 1
+    size = len(queue)
+    for _ in range(size):
+        cur = queue.popleft()
+        for i, j, u in zip(dx,dy,dz):
+            x = cur[0] + i
+            y = cur[1] + j
+            z = cur[2] + u
+            if 0 <= x < m and 0 <= y < n and 0 <= z < h and graph[z][y][x] is 0:
+                graph[z][y][x] = 1
+                queue.append((x,y,z))
+    cnt += 1
 
 if 0 in [u for i in graph for j in i for u in j]:
     print(-1)
