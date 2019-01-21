0b6c81b4da706be90d6301a4e16cff82bc071e56
kjh9267 <intellijunho@kakao.com>
2019-01-21 21:36:29 +0900

    Implementation

--- Implementation/1236.py
+++ Implementation/1236.py
@@ -1,34 +1,39 @@
-import sys
+#  성 지키기
+#  https://www.acmicpc.net/problem/1236
 
-n, m = map(int,sys.stdin.readline().split())
-graph = [list(sys.stdin.readline().rstrip()) for _ in range(n)]
-row = [False for _ in range(n)]
-col = [False for _ in range(m)]
-result = 0
+input = __import__('sys').stdin.readline
 
-for i in range(n):
-    for j in range(m):
-        if graph[i][j] == 'X':
-            row[i] = True
-            col[j] = True
+if __name__ == '__main__':
 
-for i in range(n):
-    for j in range(m):
-        if graph[i][j] == '.':
-            if row[i] is False and col[j] is False:
-                graph[i][j] = 'X'
+    n, m = map(int,input().split())
+    graph = [list(input().rstrip()) for _ in range(n)]
+    row = [False for _ in range(n)]
+    col = [False for _ in range(m)]
+    result = 0
+
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j] == 'X':
                 row[i] = True
                 col[j] = True
-                result += 1
 
-for i in range(n):
-    for j in range(m):
-        if graph[i][j] == '.':
-            if row[i] is False:
-                row[i] = True
-                result += 1
-            elif col[j] is False:
-                col[j] = True
-                result += 1
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j] == '.':
+                if row[i] is False and col[j] is False:
+                    graph[i][j] = 'X'
+                    row[i] = True
+                    col[j] = True
+                    result += 1
 
-print(result)
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j] == '.':
+                if row[i] is False:
+                    row[i] = True
+                    result += 1
+                elif col[j] is False:
+                    col[j] = True
+                    result += 1
+
+    print(result)
