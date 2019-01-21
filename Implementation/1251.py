f1360f25950d17c37a35eb48bbd5111221020aa3
kjh9267 <intellijunho@kakao.com>
2019-01-21 21:41:39 +0900

    Implementation

--- Implementation/1251.py
+++ Implementation/1251.py
@@ -1,5 +1,8 @@
 import sys
 
+#  단어 나누기
+#  https://www.acmicpc.net/problem/1251
+
 
 def convert(first,second):
     result.append(list(reversed(string[:first]))
@@ -7,14 +10,15 @@ def convert(first,second):
                   + list(reversed(string[second:])))
 
 
-string = sys.stdin.readline().rstrip()
-length = len(string)
-result = list()
+if __name__ == '__main__':
+    string = sys.stdin.readline().rstrip()
+    length = len(string)
+    result = list()
 
-for i in range(1,length-1):
-    for j in range(2,length):
-        if i >= j:
-            continue
-        convert(i,j)
+    for i in range(1,length-1):
+        for j in range(2,length):
+            if i >= j:
+                continue
+            convert(i,j)
 
-print(list(map(''.join,(sorted(result))))[0])
+    print(list(map(''.join,(sorted(result))))[0])
