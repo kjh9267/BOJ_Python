cb82262cdb79404c74d5c2632c45cf11bc95c497
kjh9267 <intellijunho@kakao.com>
2019-02-09 19:24:07 +0900

    two_pointer

--- TwoPointer/12847.py
+++ TwoPointer/12847.py
@@ -1,14 +1,15 @@
-input = __import__('sys').stdin.readline
+#  https://www.acmicpc.net/problem/12847
 
-N, M = map(int,input().split())
-data = list(map(int,input().split()))
-add = sum(data[:M])
-res = add
-for i in range(M,N):
-    # print(i, i-M, data[i], data[i-M])
-    add -= data[i-M]
-    add += data[i]
-    if res < add:
-        res = add
+if __name__ == '__main__':
+    input = __import__('sys').stdin.readline
 
-print(res)
+    N, M = map(int,input().split())
+    data = list(map(int,input().split()))
+    add = sum(data[:M])
+    res = add
+    for i in range(M,N):
+        add -= data[i-M]
+        add += data[i]
+        if res < add:
+            res = add
+    print(res)
