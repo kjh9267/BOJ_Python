eb161c51bbd4f12bd4251193d4d1341a83f57cee
kjh9267 <intellijunho@kakao.com>
2019-02-10 19:45:18 +0900

    bellman_ford

--- Bellman Ford/11657.py
+++ Bellman Ford/11657.py
@@ -1,26 +1,38 @@
+#  https://www.acmicpc.net/problem/11657
+
 import sys
 
-n, m = map(int,sys.stdin.readline().split())
-graph = [[]for _ in range(n)]
-inf = float('inf')
-res = [inf for _ in range(n)]
-res[0] = 0
 
-for _ in range(m):
-    a, b, c = map(int,sys.stdin.readline().split())
-    graph[a-1].append((b-1, c))
+class Node:
+    def __init__(self, nxt, cost):
+        self.nxt = nxt
+        self.cost = cost
 
-for i in range(n):
-    for j in range(n):
-        for node, dist in graph[j]:
-            if res[j] != inf and res[node] > res[j] + dist:
-                res[node] = res[j] + dist
-                if i == n - 1:
-                    print(-1)
-                    exit()
 
-for i in range(1,n):
-    if res[i] == 0 or res[i] == inf:
-        print(-1)
-    else:
-        print(res[i])
+if __name__ == '__main__':
+    input = sys.stdin.readline
+
+    N, M = map(int,input().split())
+    graph = [[] for _ in range(N)]
+    inf = float('inf')
+    res = [inf for _ in range(N)]
+    res[0] = 0
+
+    for _ in range(M):
+        a, b, c = map(int,input().split())
+        graph[a-1].append(Node(b-1, c))
+
+    for i in range(N):
+        for j in range(N):
+            for node in graph[j]:
+                if res[j] != inf and res[node.nxt] > res[j] + node.cost:
+                    res[node.nxt] = res[j] + node.cost
+                    if i == N - 1:
+                        print(-1)
+                        exit()
+
+    for i in range(1,N):
+        if res[i] == inf:
+            print(-1)
+        else:
+            print(res[i])
