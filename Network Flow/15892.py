c64c56574500936a0a4fcdf76e4b75f75c9c7bca
kjh9267 <intellijunho@kakao.com>
2019-02-11 20:32:10 +0900

    Dinic

--- Network Flow/15892.py
+++ Network Flow/15892.py
@@ -1,9 +1,8 @@
-import sys
-from collections import deque
+#  https://www.acmicpc.net/problem/15892
 
 
 def bfs():
-    queue = deque()
+    queue = __import__('collections').deque()
     queue.append(0)
     level[0] = 0
     while queue:
@@ -12,11 +11,11 @@ def bfs():
             if level[nxt] is -1 and capacity[cur][index] > flow[cur][index]:
                 level[nxt] = level[cur] + 1
                 queue.append(nxt)
-    return level[n-1] != -1
+    return level[N-1] != -1
 
 
 def dfs(cur,f):
-    if cur == n-1:
+    if cur == N-1:
         return f
     temp = work[cur]
     for i in range(temp,len(graph[cur])):
@@ -31,31 +30,34 @@ def dfs(cur,f):
     return 0
 
 
-n, m = map(int,sys.stdin.readline().split())
-graph = [[] for _ in range(n)]
-capacity = [[] for _ in range(n)]
-flow = [[] for _ in range(n)]
-inf = float('inf')
-total = 0
+if __name__ == '__main__':
+    input = __import__('sys').stdin.readline
 
-for _ in range(m):
-    a, b, c = map(int,sys.stdin.readline().split())
-    graph[a-1].append(b-1)
-    graph[b-1].append(a-1)
-    capacity[a-1].append(c)
-    capacity[b-1].append(c)
-    flow[a-1].append(0)
-    flow[b-1].append(0)
+    N, M = map(int,input().split())
+    graph = [[] for _ in range(N)]
+    capacity = [[] for _ in range(N)]
+    flow = [[] for _ in range(N)]
+    inf = float('inf')
+    total = 0
 
-while True:
-    level = [-1 for _ in range(n)]
-    if not bfs():
-        break
-    work = [0 for _ in range(n)]
+    for _ in range(M):
+        a, b, c = map(int,input().split())
+        graph[a-1].append(b-1)
+        graph[b-1].append(a-1)
+        capacity[a-1].append(c)
+        capacity[b-1].append(c)
+        flow[a-1].append(0)
+        flow[b-1].append(0)
+
     while True:
-        f = dfs(0,inf)
-        if not f:
+        level = [-1 for _ in range(N)]
+        if not bfs():
             break
-        total += f
+        work = [0 for _ in range(N)]
+        while True:
+            f = dfs(0,inf)
+            if not f:
+                break
+            total += f
 
-print(total)
+    print(total)
