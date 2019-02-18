a709ac7726cd728cfe8615885f477d78b1ab2ac2
kjh9267 <intellijunho@kakao.com>
2019-02-18 17:43:51 +0900

    diameter_of_tree

--- Tree/3482.py
+++ Tree/3482.py
@@ -1,3 +1,5 @@
+#  https://www.acmicpc.net/problem/3482
+
 import sys
 from collections import deque
 
@@ -8,12 +10,12 @@ def bfs(x,y):
     visit[y][x] = 0
     while queue:
         x, y = queue.popleft()
-        for i, j in zip(dx, dy):
-            xx = x + i
-            yy = y + j
-            if graph[yy][xx] == '.' and visit[yy][xx] is -1:
-                visit[yy][xx] = visit[y][x] + 1
-                queue.append((xx, yy))
+        for c, r in zip(dx, dy):
+            next_x = x + c
+            next_y = y + r
+            if graph[next_y][next_x] == '.' and visit[next_y][next_x] is -1:
+                visit[next_y][next_x] = visit[y][x] + 1
+                queue.append((next_x, next_y))
 
 
 def long():
@@ -26,36 +28,37 @@ def long():
     return temp, x, y
 
 
-T = int(sys.stdin.readline())
-dx = (1,0,-1,0)
-dy = (0,1,0,-1)
+if __name__ == '__main__':
+    T = int(sys.stdin.readline())
+    dx = (1,0,-1,0)
+    dy = (0,1,0,-1)
 
-for _ in range(T):
-    C, R = map(int, sys.stdin.readline().split())
-    graph = [list(sys.stdin.readline().rstrip()) for _ in range(R)]
-    visit = [[-1 for __ in range(C)] for _ in range(R)]
-    key = False
-    x, y = 0, 0
+    for _ in range(T):
+        C, R = map(int, sys.stdin.readline().split())
+        graph = [list(sys.stdin.readline().rstrip()) for _ in range(R)]
+        visit = [[-1 for col in range(C)] for row in range(R)]
+        key = False
+        x, y = 0, 0
 
-    for i in range(1,R-1):
-        for j in range(1,C-1):
-            cnt = 0
-            for x, y in zip(dx,dy):
-                xx = j + x
-                yy = i + y
-                if graph[yy][xx] == '#':
-                    cnt += 1
-            if cnt is 3:
-                x, y = j, i
-                key = True
+        for row in range(1,R-1):
+            for col in range(1,C-1):
+                cnt = 0
+                for x, y in zip(dx,dy):
+                    next_x = col + x
+                    next_y = row + y
+                    if graph[next_y][next_x] == '#':
+                        cnt += 1
+                if cnt is 3:
+                    x, y = col, row
+                    key = True
+                    break
+            if key:
                 break
-        if key:
-            break
 
-    bfs(x, y)
-    temp, x, y = long()
-    visit = [[-1 for __ in range(C)] for _ in range(R)]
-    bfs(x, y)
-    res, x, y = long()
+        bfs(x, y)
+        temp, x, y = long()
+        visit = [[-1 for col in range(C)] for row in range(R)]
+        bfs(x, y)
+        res, x, y = long()
 
-    print("Maximum rope length is {}.".format(res))
+        print("Maximum rope length is {}.".format(res))
