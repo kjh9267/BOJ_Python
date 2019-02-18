44f4208eb86cdce4389dec4af818b10dd5ced596
kjh9267 <intellijunho@kakao.com>
2019-02-18 19:56:33 +0900

    breadth_first_search

--- BFS/16940.py
+++ BFS/16940.py
@@ -11,7 +11,7 @@ def bfs():
         while pointer != N:
             if visited[res[pointer]]:
                 continue
-            if res[pointer] not in graph[cur]:
+            if parent[res[pointer]] != cur:
                 break
             cnt += 1
             visited[res[pointer]] = True
@@ -22,23 +22,40 @@ def bfs():
     return 0
 
 
+def parent_init():
+    queue = __import__('collections').deque()
+    queue.append(1)
+    visited[1] = True
+    while queue:
+        cur = queue.popleft()
+        for nxt in graph[cur]:
+            if visited[nxt]:
+                continue
+            parent[nxt] = cur
+            visited[nxt] = True
+            queue.append(nxt)
+
+
 if __name__ == '__main__':
     input = __import__('sys').stdin.readline
 
     N = int(input())
     graph = [[] for _ in range(N + 1)]
     visited = [False for _ in range(N + 1)]
+    parent = [0 for _ in range(N + 1)]
 
     for _ in range(N - 1):
         A, B = map(int, input().split())
         graph[A].append(B)
         graph[B].append(A)
 
-    res = list(map(int,input().split()))
+    res = list(map(int, input().split()))
     queue = __import__('collections').deque()
 
     if res[0] != 1:
         print(0)
         exit()
 
+    parent_init()
+    visited = [False for _ in range(N + 1)]
     print(bfs())
