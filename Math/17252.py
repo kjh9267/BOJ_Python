68d8b63a9dccac624d50fd62bdee8ec7b8d9f4e3
kjh9267 <intellijunho@kakao.com>
2019-07-18 18:31:44 +0900

    math

---
+++ Math/17252.py
@@ -0,0 +1,25 @@
+# https://www.acmicpc.net/problem/17252
+
+
+def solve(N):
+    if N == 0:
+        return 'NO'
+    visited = set()
+    while N > 0:
+        x = 1
+        cnt = 0
+        while x <= N:
+            x *= 3
+            cnt += 1
+        cnt -= 1
+        if cnt in visited:
+            return 'NO'
+        visited.add(cnt)
+        N -= x // 3
+    return 'YES'
+
+
+if __name__ == '__main__':
+    input = __import__('sys').stdin.readline
+    N = int(input())
+    print(solve(N))
