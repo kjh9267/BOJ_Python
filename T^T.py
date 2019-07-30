7a3e37b812956ee1316f2a89bd7bdde0ec2c7c62
kjh9267 <intellijunho@kakao.com>
2019-07-30 20:31:25 +0900

    dynamic_programming

--- T^T.py
+++ T^T.py
@@ -1,17 +1,13 @@
-# https://www.acmicpc.net/problem/11659
-
-
 if __name__ =='__main__':
     input = __import__('sys').stdin.readline
 
-    T = int(input())
-    data = ['PROBRAIN', 'GROW', 'ARGOS', 'ADMIN', 'ANT', 'MOTION', 'SPG', 'COMON', 'ALMIGHTY']
+    N , M = map(int,input().split())
 
-    maxi = 0
-    res = 0
-    for i in range(9):
-        a = max(map(int,input().split()))
-        if maxi < a:
-            maxi = a
-            res = i
-    print(data[res])
+    C = int(input())
+    C*=2
+    a = N + M
+
+    if a >= C:
+        print(a - C)
+    else:
+        print(a)
