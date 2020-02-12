662edcb9b257137c67c2b13488e3a330ec09eaea
kjh9267 <intellijunho@kakao.com>
2020-02-12 18:01:44 +0900

    refactor

--- BFS/1697.py
+++ BFS/1697.py
@@ -1,30 +1,28 @@
-import sys
-from collections import deque
+# https://www.acmicpc.net/problem/1697
 
 
-def bfs(x):
-    queue = deque()
-    queue.append(x)
+def bfs(start, target):
+    queue = __import__('collections').deque()
+    queue.append(start)
+
+    visited = [-1 for _ in range(MAX)]
+    visited[start] = 0
+
     while queue:
-        a = queue.popleft()
-        for i in (a-1,a+1,a*2):
-            if 0 <= i < 100001 and way[i] == 'x':
-                queue.append(i)
-                way[i] = a
-                if i == k:
-                    return
+        cur = queue.popleft()
+        for nxt in (cur - 1, cur + 1, cur * 2):
+            if not (0 <= nxt < MAX):
+                continue
+            if visited[nxt] != -1:
+                continue
+            visited[nxt] = visited[cur] + 1
+            queue.append(nxt)
+
+    return visited[target]
 
 
-n, k = map(int,sys.stdin.readline().split())
-graph = ['x' for _ in range(100001)]
-way = ['x' for _ in range(100001)]
-way[n] = 'o'
-cnt = 0
-
-bfs(n)
-
-while k != n:
-    cnt += 1
-    k = way[k]
-
-print(cnt)
+if __name__ == '__main__':
+    input = __import__('sys').stdin.readline
+    N, K = map(int, input().split())
+    MAX = 100_001
+    print(bfs(N, K))
