050c5f1a7ab47acdecd8cde0a334028e509282c3
kjh9267 <intellijunho@kakao.com>
2020-02-12 16:35:28 +0900

    refactor

--- Dynamic Programming/1003.py
+++ Dynamic Programming/1003.py
@@ -1,8 +1,26 @@
-dp0 = [1, 0, 1]
-dp1 = [0, 1, 1]
-for j in range(3, 41):
-    dp0.append(dp0[j - 1] + dp0[j - 2])
-    dp1.append(dp1[j - 1] + dp1[j - 2])
-for i in range(input()):
-    n = input()
-    print "{} {}".format(dp0[n],dp1[n])
+# https://www.acmicpc.net/problem/1003
+
+
+def dfs(depth):
+    if depth == 1:
+        dp[1][1] = 1
+        return dp[1]
+    if depth == 0:
+        dp[0][0] = 1
+        return dp[0]
+    if dp[depth] != [0, 0]:
+        return dp[depth]
+    dp[depth][0] = dfs(depth - 1)[0] + dfs(depth - 2)[0]
+    dp[depth][1] = dfs(depth - 1)[1] + dfs(depth - 2)[1]
+    return dp[depth]
+
+
+if __name__ == '__main__':
+    input = __import__('sys').stdin.readline
+    dp = [[0, 0] for _ in range(41)]
+    dfs(40)
+    T = int(input())
+
+    for _ in range(T):
+        num = int(input())
+        print(' '.join(map(str,dp[num])))
