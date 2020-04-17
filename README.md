0ed7048068198b02139988c8b179bb6e7e72c560
kjh9267 <intellijunho@kakao.com>
2020-04-17 21:18:45 +0900

    README

---
+++ README.md
@@ -0,0 +1,3 @@
+# 백준 온라인 저지 알고리즘 문제풀이
+
+### https://www.acmicpc.net
--- start.py
+++ start.py
@@ -1,5 +1,2 @@
 if __name__ == '__main__':
     input = __import__('sys').stdin.readline
-
-    for i in range(1, 1001):
-        print(i,end=' ')
