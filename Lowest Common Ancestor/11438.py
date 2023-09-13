1aff5e0ac3988fa1f574de834a14611f4778e853
kjh9267 <intellijunho@kakao.com>
2023-09-13 17:57:54 +0900

    LCA

--- Lowest Common Ancestor/11438.py
+++ Lowest Common Ancestor/11438.py
@@ -1,43 +1,83 @@
 # https://www.acmicpc.net/problem/11438
+from collections import deque
+from math import log
 
 
-def dfs(cur):
-    for idx, nxt in enumerate(adj):
-        if parent[nxt][0] == -1:
-            parent[nxt][0] = cur
-            depth[nxt] = depth[cur] + 1
-            dfs(nxt)
+def bfs():
+    queue = deque()
+    queue.append(_root)
 
+    visited = [-1 for _ in range(N + 1)]
+    visited[_root] = 0
 
-def fill_parent():
-    for p in range(H):
-        for idx in range(1, N):
-            if parent[idx][p] == -1:
+    while queue:
+        cur = queue.popleft()
+
+        for nxt in tree[cur]:
+            if visited[nxt] != _not_visited:
                 continue
-            parent[idx][p + 1] = parent[parent[idx][p]][p]
+            visited[nxt] = visited[cur] + 1
+            parents[nxt][0] = cur
+            queue.append(nxt)
+
+    return visited
+
+
+def init_parents():
+    for exp in range(1, log_n + 1):
+        for node in range(1, N + 1):
+            prev_node = parents[node][exp - 1]
+            parents[node][exp] = parents[prev_node][exp - 1]
+
+
+def find_lowest_common_ancestor(x, y):
+    if depth[x] < depth[y]:
+        x, y = y, x
+
+    for exp in range(log_n, -1, -1):
+        if 2 ** exp > depth[x] - depth[y]:
+            continue
+        if depth[y] > depth[parents[x][exp]]:
+            continue
+        x = parents[x][exp]
+
+    for exp in range(log_n, -1, -1):
+        if x == y:
+            break
+        if parents[x][exp] == parents[y][exp]:
+            continue
+        x = parents[x][exp]
+        y = parents[y][exp]
+
+    if x == y:
+        return x
+    return parents[x][0]
 
 
 if __name__ == '__main__':
     input = __import__('sys').stdin.readline
+    _root = 1
+    _not_visited = -1
+    _new_line = '\n'
     N = int(input())
-    H = 1
+    tree = [list() for _ in range(N + 1)]
+    log_n = int(log(N, 2))
+    parents = [[0 for _ in range(log_n + 1)] for _ in range(N + 1)]
 
-    while H < N:
-        H <<= 1
+    for _ in range(N - 1):
+        start, end = map(int, input().split())
+        tree[start].append(end)
+        tree[end].append(start)
 
-    adj = [list() for _ in range(N)]
-    parent = [[-1 for _ in range(H)] for _ in range(N)]
-    depth = [-1 for _ in range(N)]
-    depth[0] = 0
+    depth = bfs()
+    init_parents()
 
-    for _ in range(N):
-        u, v = map(lambda x: int(x) - 1, input().split())
-        adj[u].append(v)
-        adj[v].append(u)
-
-    dfs(0)
-    fill_parent()
     M = int(input())
+    result = deque()
 
     for _ in range(M):
-        u, v = map(lambda x: int(x) - 1, input().split())
+        x, y = map(int, input().split())
+        lowest_common_ancestor = find_lowest_common_ancestor(x, y)
+        result.append(lowest_common_ancestor)
+
+    print(_new_line.join(map(str, result)))
