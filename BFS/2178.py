6f0b852d3a42fc926807ed511e8a12d334d174ac
kjh9267 <intellijunho@kakao.com>
2025-03-20 19:35:44 +0900

    refactor: bfs, python3

--- BFS/2178.py
+++ BFS/2178.py
@@ -1,31 +1,43 @@
-def bfs(graph,visit):
-    queue = [[1,1]]
-    level = []
-    cnt = 0
-    while len(queue) is not 0:
-        for i in range(len(queue)):
-            visit[queue[i][0]-1][queue[i][1]-1] = 1
-            if [n, m] in queue:
-                break
-            if queue[i][0] - 1 >= 1:
-                if graph[queue[i][0]-2][queue[i][1]-1] == 1 and visit[queue[i][0]-2][queue[i][1]-1] == 0 and [queue[i][0] - 1,queue[i][1]] not in level:
-                    level.append([queue[i][0] - 1,queue[i][1]])
-            if queue[i][1] - 1 >= 1:
-                if graph[queue[i][0]-1][queue[i][1]-2] == 1 and visit[queue[i][0]-1][queue[i][1]-2] == 0 and [queue[i][0],queue[i][1]-1] not in level:
-                    level.append([queue[i][0],queue[i][1]-1])
-            if queue[i][0] + 1 <= len(graph):
-                if graph[queue[i][0]][queue[i][1]-1] == 1 and visit[queue[i][0]][queue[i][1]-1] == 0 and [queue[i][0] + 1,queue[i][1]] not in level:
-                    level.append([queue[i][0] + 1,queue[i][1]])
-            if queue[i][1] + 1 <= len(graph[0]):
-                if graph[queue[i][0]-1][queue[i][1]] == 1 and visit[queue[i][0]-1][queue[i][1]] == 0 and [queue[i][0],queue[i][1]+1] not in level:
-                    level.append([queue[i][0],queue[i][1]+1])
-        queue = []
-        if len(queue) is 0:
-            queue = level
-            level = []
-        cnt += 1
-    return cnt
-n, m = map(int,raw_input().split())
-graph = [map(int,list(raw_input())) for i in range(n)]
-visit = [[0 for j in range(m)] for i in range(n)]
-print bfs(graph,visit)
+# https://www.acmicpc.net/problem/2178
+
+from collections import deque
+
+
+def bfs():
+    queue = deque()
+    queue.append((_start_x, _start_y))
+
+    visited = [[_not_visited for _ in range(M)] for _ in range(N)]
+    visited[_start_y][_start_x] = 0
+
+    while queue:
+        x, y = queue.popleft()
+
+        for diff_x, diff_y in zip(dx, dy):
+            next_x = x + diff_x
+            next_y = y + diff_y
+
+            if not (0 <= next_x < M and 0 <= next_y < N):
+                continue
+            if grid[next_y][next_x] == _wall:
+                continue
+            if visited[next_y][next_x] != _not_visited:
+                continue
+            visited[next_y][next_x] = visited[y][x] + 1
+            queue.append((next_x, next_y))
+
+    return visited[N - 1][M - 1] + 1
+
+
+if __name__ == '__main__':
+    input = __import__('sys').stdin.readline
+    _not_visited = -1
+    _wall = '0'
+    _start_x = 0
+    _start_y = 0
+    dx = (1, 0, -1, 0)
+    dy = (0, 1, 0, -1)
+    N, M = map(int, input().split())
+    grid = [input().rstrip() for _ in range(N)]
+
+    print(bfs())
