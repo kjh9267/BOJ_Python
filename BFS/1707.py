cd5674a5403cd6c34f9c103429a0723d02639f36
kjh9267 <intellijunho@kakao.com>
2025-03-20 22:08:47 +0900

    bfs

--- BFS/1707.py
+++ BFS/1707.py
@@ -1,35 +1,58 @@
+# https://www.acmicpc.net/problem/1707
 import sys
 from collections import deque
 
-k = int(sys.stdin.readline())
+sys.setrecursionlimit(999999999)
 
-for _ in range(k):
-    v, e = map(int,sys.stdin.readline().split())
-    graph = [[] for _ in range(v)]
-    visit = [0 for _ in range(v)]
 
-    for __ in range(e):
-        a, b = map(int,sys.stdin.readline().split())
-        graph[a-1].append(b-1)
-        graph[b-1].append(a-1)
+def bfs(start, visited):
+    queue = deque()
+    queue.append(start)
 
-    key = False
-    for j in range(v):
-        if visit[j] is 0:
-            queue = deque()
-            queue.append(j)
-            visit[j] = 1
-            while queue:
-                cur = queue.popleft()
-                for i in graph[cur]:
-                    if visit[i] is 0:
-                        visit[i] = -visit[cur]
-                        queue.append(i)
-                    elif visit[i] is visit[cur]:
-                        key = True
-                        break
+    visited[start] = 1
 
-    if key:
-        print('NO')
-    else:
-        print('YES')
+    while queue:
+        cur = queue.popleft()
+
+        for nxt in graph[cur]:
+            if visited[nxt] != _not_visited:
+                if visited[nxt] == visited[cur]:
+                    return True
+                continue
+            visited[nxt] = -visited[cur]
+            queue.append(nxt)
+
+    return False
+
+
+if __name__ == '__main__':
+    input = __import__('sys').stdin.readline
+    _yes = 'YES'
+    _no = 'NO'
+    _new_line = '\n'
+    _not_visited = 0
+    result = list()
+    K = int(input())
+
+    for _ in range(K):
+        V, E = map(int, input().split())
+        graph = [list() for _ in range(V + 1)]
+        visited = [_not_visited for _ in range(V + 1)]
+
+        for _ in range(E):
+            start, end = map(int, input().split())
+            graph[start].append(end)
+            graph[end].append(start)
+
+        impossible = False
+        for start in range(1, V + 1):
+            if visited[start] != _not_visited:
+                continue
+            impossible |= bfs(start, visited)
+
+        if impossible:
+            result.append(_no)
+        else:
+            result.append(_yes)
+
+    print(_new_line.join(result))
